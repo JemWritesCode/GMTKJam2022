f1ad994e33af401415ea9e97ab7e70da8cc2c9c4
redseiko <redseiko@gmail.com>
2022-07-16 12:50:42 -0700

    Extract Hittables death logic to use an EventHandler and move the code into SetupHittables script.
    Create a HittableThingsManager and move the SetupHittables thing there so that it also has reference to prefabs.

--- GMTKJamProject/Assets/Scripts/SetupHittables.cs
+++ GMTKJamProject/Assets/Scripts/SetupHittables.cs
@@ -1,6 +1,9 @@
 using UnityEngine;
 
 public class SetupHittables : MonoBehaviour {
+  public JazzHands playerJazzHands;
+  public GameObject hittableDeathExplosion;
+
   void Awake() {
     GameObject[] hittables = GameObject.FindGameObjectsWithTag("Hittable");
     int rigidBodyAdded = 0;
@@ -14,13 +17,22 @@ public class SetupHittables : MonoBehaviour {
 
       rigidbody.collisionDetectionMode = CollisionDetectionMode.Continuous;
 
-      if (!hittable.TryGetComponent(out HittableThing _)) {
-        hittable.AddComponent<HittableThing>();
+      if (!hittable.TryGetComponent(out HittableThing hittableThing)) {
+        hittableThing = hittable.AddComponent<HittableThing>();
         hittableThingAdded++;
       }
+
+      hittableThing.OnHittableDeath += (_, hittable) => AddExplosion(hittable);
+      hittableThing.OnHittableDeath += (_, _) => playerJazzHands.IncreaseHandPower(1f);
     }
 
     Debug.Log(
         $"Added {rigidBodyAdded} Rigidbody and {hittableThingAdded} HittableThing to {hittables.Length} Hittable.");
+  }
+
+  void AddExplosion(GameObject hittable) {
+    Debug.Log($"Adding explosion to {hittable.name}");
+    GameObject explosion = Instantiate(hittableDeathExplosion, hittable.transform);
+    explosion.SetActive(true);
   }
 }
