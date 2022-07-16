1f5d2a3504081dda324e7f636ad370e2510f1d52
redseiko <redseiko@gmail.com>
2022-07-15 19:16:25 -0700

    Rename some fields in HittableThing and add new fields healthMax and healthCurrent.

--- GMTKJamProject/Assets/HittableThing.cs
+++ GMTKJamProject/Assets/HittableThing.cs
@@ -1,8 +1,11 @@
 using UnityEngine;
 
 public class HittableThing : MonoBehaviour {
-  public float AddRelativeForceOnCollideMultiplier = 300f;
-  public float AddUpwardsForceOnCollideMultiplier = 200f;
+  public float healthMax = 300f;
+  public float healthCurrent = 300f;
+
+  public float onCollideAddRelativeForcePower = 300f;
+  public float onCollideAddUpwardsForcePower = 200f;
 
   Rigidbody _rigidBody;
 
@@ -13,11 +16,10 @@ public class HittableThing : MonoBehaviour {
   private void OnCollisionEnter(Collision collision) {
     if (collision.gameObject.CompareTag("Player")) {
       Vector3 force = collision.contacts[0].point - transform.position;
-      force = -force.normalized;
-      force = force * AddRelativeForceOnCollideMultiplier + Vector3.up * AddUpwardsForceOnCollideMultiplier;
+      force = force.normalized;
+      force = force * -onCollideAddRelativeForcePower + Vector3.up * onCollideAddUpwardsForcePower;
 
       Debug.Log($"{collision.gameObject.name} hit me with {collision.contactCount} contacts, adding force: {force}");
-
       _rigidBody.AddForce(force);
     }
   }
