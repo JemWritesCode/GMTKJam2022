8b229aeb99c57852a896cb275e0084a59d33cb71
redseiko <redseiko@gmail.com>
2022-07-15 19:39:56 -0700

    Temporarily add a very bad explosion that triggers when an object dies.

--- GMTKJamProject/Assets/HittableThing.cs
+++ GMTKJamProject/Assets/HittableThing.cs
@@ -1,16 +1,29 @@
+using System.Linq;
+
 using UnityEngine;
 
 public class HittableThing : MonoBehaviour {
   public float healthMax = 300f;
   public float healthCurrent = 300f;
 
+  public float healthDamageOnCollide = 150f;
+
   public float onCollideAddRelativeForcePower = 300f;
   public float onCollideAddUpwardsForcePower = 200f;
 
   Rigidbody _rigidBody;
+  GameObject _explosion;
 
   void Awake() {
     _rigidBody = GetComponent<Rigidbody>();
+    _explosion =
+        FindObjectsOfType<ParticleSystem>(includeInactive: true)
+            .Where(ps => ps.name == "HittableExplosion")
+            .Select(ps => ps.gameObject).FirstOrDefault();
+
+    if (!_explosion) {
+      Debug.LogError($"Could not find _explosion.");
+    }
   }
 
   private void OnCollisionEnter(Collision collision) {
@@ -24,5 +37,14 @@ public class HittableThing : MonoBehaviour {
 
     Debug.Log($"{collision.gameObject.name} hit me with {collision.contactCount} contacts, adding force: {force}");
     _rigidBody.AddForce(force);
+
+    healthCurrent -= healthDamageOnCollide;
+
+    if (healthCurrent <= 0f && _explosion) {
+      Debug.Log($"Adding explosion");
+      GameObject explosion = Instantiate(_explosion, transform);
+      explosion.SetActive(true);
+      Destroy(gameObject, 2f);
+    }
   }
 }
