eed99099ef34e6cf77062329018048329dba6d31
redseiko <redseiko@gmail.com>
2022-07-17 11:15:14 -0700

    Add left-click to wave.

--- GMTKJamProject/Assets/Scripts/JazzHands.cs
+++ GMTKJamProject/Assets/Scripts/JazzHands.cs
@@ -8,4 +8,24 @@ public class JazzHands : MonoBehaviour {
     leftHand.transform.localScale = Vector3.one * scale;
     rightHand.transform.localScale = Vector3.one * scale;
   }
+
+  public Animator animator;
+
+  void Awake() {
+    if (!animator) {
+      animator = GetComponent<Animator>();
+    }
+  }
+
+  void Update() {
+    if (Input.GetMouseButtonDown(0) && !IsPlaying(1, "Wave")) {
+      animator.SetTrigger("Wave");
+    }
+  }
+
+  bool IsPlaying(int layerIndex, string stateName) {
+    return
+        animator.GetCurrentAnimatorStateInfo(layerIndex).IsName(stateName)
+        && animator.GetCurrentAnimatorStateInfo(layerIndex).normalizedTime < 1.0f;
+  }
 }
