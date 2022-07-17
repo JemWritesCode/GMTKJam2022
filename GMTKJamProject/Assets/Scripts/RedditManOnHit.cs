317f73405eb0463c9774d380006f718dc117448a
redseiko <redseiko@gmail.com>
2022-07-16 21:15:44 -0700

    Uhhh something.

--- GMTKJamProject/Assets/Scripts/RedditManOnHit.cs
+++ GMTKJamProject/Assets/Scripts/RedditManOnHit.cs
@@ -1,18 +1,54 @@
+using Unity.VisualScripting;
+
 using UnityEngine;
 
 public class RedditManOnHit : MonoBehaviour {
   public CameraController cameraController;
   public GameObject player;
-  public float triggerRadius = 75f;
+  public float triggerRadius = 2f;
 
   void Update() {
     if (Vector3.Distance(transform.position, player.transform.position) <= triggerRadius) {
-      cameraController.cameraTarget = transform;
+      //cameraController.cameraTarget = transform;
+      TogglePanToDude();
       UIManager.Instance.ToggleInvestFundsPanel(true);
     } else {
-      cameraController.cameraTarget = player.transform;
+      //cameraController.cameraTarget = player.transform;
+      cameraController.enabled = true;
+      panToDude = false;
+      alreadyPanned = false;
       UIManager.Instance.ToggleInvestFundsPanel(false);
     }
+  }
+
+  bool panToDude = false;
+  bool alreadyPanned = false;
+
+  void TogglePanToDude() {
+    if (alreadyPanned) {
+      return;
+    }
+
+    cameraController.enabled = false;
+    alreadyPanned = true;
+    panToDude = true;
+  }
+
+  public float cameraFocusHeight = 1f;
+  public Quaternion cameraFocusRotation = Quaternion.Euler(0f, 180f, 0f);
+  public float cameraFocusDistance = 2f;
+
+  void LateUpdate() {
+    if (!panToDude) {
+      return;
+    }
+
+    Vector3 position = transform.position;
+    position -= cameraFocusRotation * Vector3.forward * cameraFocusDistance;
+    position.y = transform.position.y + cameraFocusHeight;
+
+    cameraController.transform.position = position;
+    cameraController.transform.LookAt(transform.position + new Vector3(0, cameraFocusHeight, 0));
   }
 
   //void OnCollisionEnter(Collision collision) {
